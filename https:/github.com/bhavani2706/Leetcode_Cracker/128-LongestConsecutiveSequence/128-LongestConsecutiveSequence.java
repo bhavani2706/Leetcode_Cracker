d3a4e8e79681d69a6de5742735d46121c1fe121f
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:50:08 +0530

    Create 128-LongestConsecutiveSequence

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 09:50:08
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        if(n==0)return 0;
+        int curr_count=1,max_count=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1])continue;
+            if(nums[i]-1==nums[i-1]) curr_count++;
+            else{
+                max_count=Math.max(max_count,curr_count);
+                curr_count=1;
+            }
+        }
+        return Math.max(max_count,curr_count);
+      
+     
+    }
+}
