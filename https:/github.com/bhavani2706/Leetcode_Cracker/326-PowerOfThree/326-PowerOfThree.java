c8120f077ec6cf4602da0e2f34a835571e460cfe
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:11 +0530

    Create 326-PowerOfThree

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/326-PowerOfThree/326-PowerOfThree.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 09:49:10
+class Solution {
+    public boolean isPowerOfThree(int n) {
+         if(n==1)
+                return true;
+                 if(n%3!=0 || n<=0)
+             return false;
+        
+            
+            
+            
+        
+        return isPowerOfThree(n/3);
+    }
+}
