97a663779a11bd0b18ceca84e6425422167a47f8
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:02:20 +0530

    Create 826-SoupServings

---
+++ 826-SoupServings/826-SoupServings.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 10:02:20
+class Solution {
+    public double soupServings(int N) {
+        if (N >= 5000) return 1.0;  
+         int n = (N + 24) / 25; 
+        Double[][] memo = new Double[n + 1][n + 1];
+        return dfs(n, n, memo);
+    }
+private double dfs(int a, int b, Double[][] memo) {
+        if (a <= 0 && b <= 0) return 0.5;  
+        if (a <= 0) return 1.0;           
+        if (b <= 0) return 0.0;            
+     if (memo[a][b] != null) return memo[a][b];
+        memo[a][b] = 0.25 * (
+            dfs(a - 4, b, memo) +
+            dfs(a - 3, b - 1, memo) +
+            dfs(a - 2, b - 2, memo) +
+            dfs(a - 1, b - 3, memo)
+        );
+ return memo[a][b];
+    }
+}
