73d782793c9e740224bbc68f3cc6884eca2def21
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:41 +0530

    Create 198-HouseRobber

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,13 @@
+// Last updated: 09/07/2026, 09:49:40
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        
+        int arr[]=new int[n+1];
+        arr[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            arr[i]=Math.max(arr[i-1],arr[i-2]+nums[i-1]);
+        }return arr[n];
+        
+    }
+}
