a5a0af7e89beadcd0a329b05fd1fd69f8dc686b4
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:59 +0530

    Create 147-InsertionSortList

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/147-InsertionSortList/147-InsertionSortList.java
@@ -0,0 +1,38 @@
+// Last updated: 09/07/2026, 09:49:59
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return null;
+
+        ListNode dummy = new ListNode(0); // Dummy node for sorted list
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next; // Save next node to process
+
+            // Find position in sorted part
+            ListNode prev = dummy;
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+
+            // Insert curr between prev and prev.next
+            curr.next = prev.next;
+            prev.next = curr;
+
+            // Move to the next node
+            curr = nextNode;
+        }
+
+        return dummy.next; // Head of sorted list
+    }
+}
