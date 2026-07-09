a3046c24d1d17bb33438dc1561968781f595bdc0
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:47:26 +0530

    Create 1603-RunningSumOf1dArray

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:47:26
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+    
+            nums[i]=nums[i]+nums[i-1];
+        }
+        return nums;
+        
+    }
+}
